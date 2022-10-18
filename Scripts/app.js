54e4178e8689e61682754ddc89a12a6179f4c5a3
Reeja Mathew <200547300@student.georgianc.on.ca>
2022-10-18 11:03:23 -0400

    commit changing to type script

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,28 +1,24 @@
 // IIFE - Immediately Invoked Function Express
 //AKA - Anonymous Self-Executing Function
-//"use strict"
-(function(){
-
+"use strict";
+(function () {
     //function format 1 - named function
-   /* function Start(){
-
+    function Start() {
+        console.log("App Started");
     }
-*/
-/*
-
-// function format 2 - anonymous function - pointed at a by variable
-let Start = function(){
-
-}*/
-
-// function format 3 - anonymous function -> arrow version
-let Start = () =>
-{
-    console.log("App Started")
-
-}
-
-
-
-    window.addEventListener("load",Start)
+    /*
+    
+    // function format 2 - anonymous function - pointed at a by variable
+    let Start = function(){
+    
+    }*/
+    // function format 3 - anonymous function -> arrow version
+    /*let Start = () =>
+    {
+        console.log("App Started")
+    
+    }
+    */
+    window.addEventListener("load", Start);
 })();
+//# sourceMappingURL=app.js.map
---
+++ Scripts/app.ts
@@ -0,0 +1,29 @@
+// IIFE - Immediately Invoked Function Express
+//AKA - Anonymous Self-Executing Function
+"use strict";
+(function(){
+
+    //function format 1 - named function
+   function Start(){
+
+    console.log("App Started")
+    }
+
+/*
+
+// function format 2 - anonymous function - pointed at a by variable
+let Start = function(){
+
+}*/
+
+// function format 3 - anonymous function -> arrow version
+/*let Start = () =>
+{
+    console.log("App Started")
+
+}
+*/
+
+
+    window.addEventListener("load",Start)
+})();
